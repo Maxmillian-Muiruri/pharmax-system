f288760b4a8b55b2f893b4e49b8e78e44095a24c
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2026-05-27 13:37:09 +0300

    delete files

--- .kilo/worktrees/lunar-liver/apps/pharmaOS-main (3)/frontend/src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(amount) {
-  return new Intl.NumberFormat('en-KE', {
-    style: 'currency',
-    currency: 'KES',
-    minimumFractionDigits: 2,
-  }).format(amount)
-}
